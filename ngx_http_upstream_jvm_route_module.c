8d0df84ebae353bc7c7db7a062fadcc5137e898c
IvanDvoeglazov <80915999+IvanDvoeglazov@users.noreply.github.com>
2024-10-10 08:27:09 +0000

    1

--- ngx_http_upstream_jvm_route_module.c
+++ ngx_http_upstream_jvm_route_module.c
@@ -1073,8 +1073,8 @@ ngx_http_upstream_jvm_route_set_session(ngx_peer_connection_t *pc, void *data)
     rc = ngx_ssl_set_session(pc->connection, ssl_session);
 
     ngx_log_debug2(NGX_LOG_DEBUG_HTTP, pc->log, 0,
-                   "set session: %p:%d",
-                   ssl_session, ssl_session ? ssl_session->references : 0);
+                   "set session: %p",
+                   ssl_session);
 
     /* ngx_unlock_mutex(jrp->peers->mutex); */
 
@@ -1099,7 +1099,7 @@ ngx_http_upstream_jvm_route_save_session(ngx_peer_connection_t *pc, void *data)
     }
 
     ngx_log_debug2(NGX_LOG_DEBUG_HTTP, pc->log, 0,
-                   "save session: %p:%d", ssl_session, ssl_session->references);
+                   "save session: %p", ssl_session);
 
     peer = &jrp->peers->peer[jrp->current];
 
@@ -1114,8 +1114,8 @@ ngx_http_upstream_jvm_route_save_session(ngx_peer_connection_t *pc, void *data)
     if (old_ssl_session) {
 
         ngx_log_debug2(NGX_LOG_DEBUG_HTTP, pc->log, 0,
-                       "old session: %p:%d",
-                       old_ssl_session, old_ssl_session->references);
+                       "old session: %p",
+                       old_ssl_session);
 
         /* TODO: may block */
 
